bf683c73b82ec63fb594f42ed6914571648f3538
Diego <diegoramirez12ok@gmail.com>
2025-11-21 20:03:15 -0300

    Agregar modelo User con encriptacion de pw usando bcrypt

---
+++ backend/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({ 
+
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
+  role: {
+    type: String,
+    enum: ['usuario', 'admin'],
+    default: 'usuario'
+  }
+}, {
+  timestamps: true
+});
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+
+
+
+
+
