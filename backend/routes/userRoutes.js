b36b26f72a5edcbddd07318a8a0492483100e254
Diego <diegootorres2010@hotmail.com>
2025-11-27 17:23:30 -0300

    Solucion de conflicto entre rutas

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const userController = require('../controllers/userControllers');
 const authMiddleware = require('../middlewares/auth');
 
+router.get("/profile", authMiddleware, userController.getUserProfile);
 
-router.post("/", userController.createUser)
+router.post("/", userController.createUser);
 router.get('/', userController.getUsers);
+
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-router.get("/profile", authMiddleware, userController.getUserProfile)
+
+
 
 module.exports = router;
