73dd9caa786bd10939a9129dc6285c8ba5a879df
Diego <diegootorres2010@hotmail.com>
2025-11-27 16:59:21 -0300

    resuelvo conflictos de compatibilidad, redeploy para pruebas en render

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,11 @@ const userController = require('../controllers/userControllers');
 const authMiddleware = require('../middlewares/auth');
 
 
-router.post("/", authMiddleware, userController.createUser)
+router.post("/", userController.createUser)
 router.get('/', userController.getUsers);
 router.get('/:id', userController.getUserById);
 router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
-router.get("/perfil", authMiddleware, userController.getUserProfile)
+router.get("/profile", authMiddleware, userController.getUserProfile)
 
 module.exports = router;
