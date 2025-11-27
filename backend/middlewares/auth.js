594069603ce2cb3d5ad35166d41c634ee7b09fc9
Diego <diegootorres2010@hotmail.com>
2025-11-27 19:06:14 -0300

    prueba

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -9,11 +9,12 @@ module.exports = (req, res, next) => {
 
   const token = header.split(" ")[1];
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch (error) {
-    return res.status(401).json({ msg: "Token inválido." });
-  }
+try {
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  console.log("🟢 Token decodificado:", decoded);
+  req.user = decoded;
+  next();
+} catch (error) {
+  return res.status(401).json({ msg: "Token inválido." });
+}
 };
